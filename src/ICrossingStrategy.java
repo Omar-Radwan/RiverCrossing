7533d15b23210fc6f6a782edc4327189a98c42a0
Omar Radwan <Omarradwanx@gmail.com>
2019-04-21 04:56:16 +0200

    Initial commit

---
+++ src/ICrossingStrategy.java
@@ -0,0 +1,9 @@
+import java.util.List;
+
+public interface ICrossingStrategy {
+
+	public boolean isValid (List<ICrosser> rightBankCrossers,List<ICrosser> leftBankCrossers,List<ICrosser> boatRiders);
+	public List<ICrosser> getInitialCrossers();
+	public String [] getInstructions();
+	
+}
---
+++ src/IRiverCrossingController.java
@@ -0,0 +1,23 @@
+import java.util.List;
+
+public interface IRiverCrossingController {
+	public void newGame (ICrossingStrategy gameStrategy);
+	public void resetGame();
+	public String [] getInstructions();
+	public List<ICrosser> getCrossersOnRightBank();
+	public List<ICrosser> getCrosserOnLeftBank();
+	public boolean isBoatOnTheLeftBank();
+	public int getNumberOfSails();
+	public boolean canMove(List<ICrosser> crossers, boolean fromLeftToRightBank);
+	public void doMove(List<ICrosser> crossers,boolean fromLeftToRightBank);
+	public boolean canUndo();
+	public boolean canRedo();
+	public void undo ();
+	public void redo();
+	public void saveGame();
+	public void loadGame();
+	public List<List<ICrosser>> solveGame();
+
+
+
+}
