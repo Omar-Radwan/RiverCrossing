a45a6fdd581ee3e8b1a7becb654ad9861248e824
ZiadElZawawy <ziadhossamdo@hotmail.com>
2019-04-22 23:39:18 +0200

    gui

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 import crossers.CrossersFactory;
 import crossers.ICrosser;
 import crossers.concreteclasses.Farmer;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class Main {
+public class Main extends Application {
 
 	public static void main(String[] args) {
 
@@ -10,8 +12,14 @@ public class Main {
 		
 		ICrosser f1 = crossersFactory.getCrosser("farmer");
 
-		
+		launch(args);
 	
+		
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
 		
 	}
 
---
+++ src/gui/related/IGameLevel.java
@@ -0,0 +1,5 @@
+package gui.related;
+
+public interface IGameLevel {
+
+}
